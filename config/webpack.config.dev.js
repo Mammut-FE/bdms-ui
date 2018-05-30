8905ac6ccea2e72454d6717c8bb4673d24bf3a8c
lleohao <lleohao@hotmail.com>
2018-05-30 19:19:47 +0800

    feat(component): 修改checkbox组件样式

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -164,7 +164,8 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 2,
-                  modules: true
+                  modules: true,
+                  localIdentName: '[name]__[local]--[hash:base64:5]'
                 }
               },
               {
--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -1,50 +1,52 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import './checkbox.scss'
-import '../../style/index.scss'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './checkbox.scss';
+import '../../style/index.scss';
+
+const cx = classNames.bind(styles);
 
 export default class Checkbox extends Component {
-    onChange = (e) => {
-        const checked = e.target.checked;
-        const { onChange } = this.props;
-
-        if (onChange) {
-            onChange(this.props.value, checked)
-        }
+  onChange = (e) => {
+    const checked = e.target.checked;
+    const {onChange} = this.props;
+    
+    if (onChange) {
+      onChange(this.props.value, checked);
     }
-
-    render() {
-        const {children, value, disabled, className, isIndeterminate, checked} = this.props
-
-        const lableClass = classNames('u-label', className)
-        const inputClass = classNames('u-checkbox-input', {
-            'checked': checked,
-            'disabled-on': disabled && checked,
-            'disabled-off': disabled && !checked,
-            'indeterminate': isIndeterminate
-        })
-
-        return (
-            <label className={lableClass}>
-                <span className={inputClass}>
-                    <span className="u-checkbox-inner"></span>
-                    <input
-                    type="checkbox"
-                    checked={checked}
-                    disabled={disabled}
-                    onChange={this.onChange}
-                    />
-                </span>
-                <span className='u-checkbox-text'>{children || value}</span>
-            </label>
-        )
-    }
+  };
+  
+  render () {
+    const {children, value, disabled, className, isIndeterminate, checked} = this.props;
+    
+    const labelClass = cx('u-label', className);
+    const inputClass = cx('u-checkbox-input', {
+      'checked': checked,
+      'disabled-on': disabled && checked,
+      'disabled-off': disabled && !checked,
+      'indeterminate': isIndeterminate
+    });
+    
+    return (
+      <label className={labelClass}>
+        <span className={inputClass}>
+          <span className={cx('u-checkbox-inner')}/>
+          <input
+            type="checkbox"
+            checked={checked}
+            disabled={disabled}
+            onChange={this.onChange}
+          />
+        </span>
+        <span className={cx('u-checkbox-text')}>{children || value}</span>
+      </label>
+    );
+  }
 }
 
 Checkbox.propTypes = {
-    value: PropTypes.string,
-    checked: PropTypes.bool,
-    disabled: PropTypes.bool,
-    isIndeterminate: PropTypes.bool
-}
+  value: PropTypes.string,
+  checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  isIndeterminate: PropTypes.bool
+};
