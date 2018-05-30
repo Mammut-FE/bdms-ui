01ea9714bfc05f764da3b88d6fa9ae347e24b663
mengera88 <1158584767@qq.com>
2018-05-30 20:00:59 +0800

    格式化

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,201 +2,194 @@
  * @Author: jessica(hzgujing@corp.netease.com)
  * @Date: 2018-01-05 16:12:23
  * @Last Modified by: jessica(hzgujing@corp.netease.com)
- * @Last Modified time: 2018-01-15 17:17:05
+ * @Last Modified time: 2018-05-30 20:00:37
  */
-import React, { Component} from 'react'
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types'
-import menuClass from './menu.scss'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import menuClass from "./menu.scss";
 
-import Icon from '../icon'
-import MenuContent from './MenuContent'
-import classNames from 'classnames/bind'
-import _ from 'lodash'
+import Icon from "../icon";
+import MenuContent from "./MenuContent";
+import classNames from "classnames/bind";
+import _ from "lodash";
 
-const SCROLL_UNIT = 400
+const SCROLL_UNIT = 400;
 
 export default class Menu extends Component {
-    constructor(props) {
-        super (props)
-        this.state = {
-            selected: this.getSelected(props),
-            menuItems: {},
-            overflowTop: false,
-            overflowBottom: false,
-            offsetHeight: 0,
-            scrollHeight: 0,
-            scrollTop: 0,
-            baseScrollTop: 0,
-            checkItem: []
-        }
-        this.menuContent = null
-        this.handleCheck = this.handleCheck.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: this.getSelected(props),
+      menuItems: {},
+      overflowTop: false,
+      overflowBottom: false,
+      offsetHeight: 0,
+      scrollHeight: 0,
+      scrollTop: 0,
+      baseScrollTop: 0,
+      checkItem: []
+    };
+    this.menuContent = null;
+    this.handleCheck = this.handleCheck.bind(this);
+  }
 
-    getSelected(props) {
-        return props.selected
-    }
+  getSelected(props) {
+    return props.selected;
+  }
 
-    setMenuItems(instance) {
-        let menuItems = Object.assign({}, this.state.menuItems);
-        menuItems[instance.props.command] = instance;
-        this.setState({
-            menuItems: menuItems
+  setMenuItems(instance) {
+    let menuItems = Object.assign({}, this.state.menuItems);
+    menuItems[instance.props.command] = instance;
+    this.setState({
+      menuItems: menuItems
+    });
+  }
+
+  getChildContext() {
+    return {
+      component: this
+    };
+  }
+
+  handleMenuItemClick(key, instance) {
+    if (this.props.onCommand) {
+      this.props.onCommand(key, instance);
+      this.setState({
+        selected: key
+      });
+    }
+  }
+
+  overflowBottomControl(overflow) {
+    this.setState({
+      overflowBottom: overflow
+    });
+  }
+
+  overflowTopControl(overflow) {
+    this.setState({
+      overflowTop: overflow
+    });
+  }
+
+  setScrollTop(scrollTop) {
+    this.setState({
+      scrollTop: scrollTop
+    });
+  }
+
+  setBaseScrollTop(scrollTop) {
+    this.setState({
+      baseScrollTop: scrollTop
+    });
+  }
+
+  getMenuContent(content) {
+    this.menuContent = content;
+  }
+
+  scrollUp() {
+    let scrollTop = this.state.scrollTop - SCROLL_UNIT;
+    if (scrollTop < 0) {
+      scrollTop = 0;
+    }
+    this.setScrollTop(scrollTop);
+    this.slideScroll(scrollTop);
+  }
+
+  scrollDown() {
+    const el = ReactDOM.findDOMNode(this.menuContent);
+    let scrollTop = this.state.scrollTop + SCROLL_UNIT;
+    if (scrollTop > el.scrollHeight - el.offsetHeight) {
+      scrollTop = el.scrollHeight - el.offsetHeight;
+    }
+    this.setScrollTop(scrollTop);
+    this.slideScroll(scrollTop);
+  }
+
+  slideScroll(scrollTop) {
+    const el = ReactDOM.findDOMNode(this.menuContent);
+    let time = setInterval(() => {
+      if (el.scrollTop < scrollTop) {
+        el.scrollTop += 40;
+      } else if (el.scrollTop === scrollTop) {
+        clearInterval(time);
+      } else if (el.scrollTop > scrollTop) {
+        el.scrollTop -= 40;
+      }
+    }, 5);
+  }
+
+  handleCheck(command, checked) {
+    let checkItem = this.state.checkItem;
+    if (checked) {
+      checkItem.push(command);
+    } else {
+      checkItem.splice(_.indexOf(command), 1);
+    }
+    this.setState({
+      checkItem: checkItem
+    });
+
+    if (this.props.onCheck) {
+      this.props.onCheck(checkItem);
+    }
+  }
+
+  render() {
+    const { className, children, style } = this.props;
+    const { overflowBottom, overflowTop, scrollTop, selected } = this.state;
+
+    let cx = classNames.bind(menuClass);
+
+    const menuClasses = cx("u-menu", className, {
+      "pdb-change": overflowBottom,
+      "pdt-change": overflowTop
+    });
+
+    const childrenWithProps = React.Children.map(children, (child, index) => {
+      if (!child) {
+        return null;
+      }
+
+      return React.cloneElement(
+        child,
+        Object.assign({}, child.props, {
+          selected: selected
         })
-    }
+      );
+    });
 
-
-    getChildContext() {
-        return {
-            component: this
-        }
-    }
-
-    handleMenuItemClick(key, instance) {
-        if (this.props.onCommand) {
-            this.props.onCommand(key, instance);
-            this.setState({
-                selected: key
-            })
-        }
-    }
-
-    overflowBottomControl(overflow) {
-        this.setState({
-            overflowBottom: overflow
-        })
-    }
-
-    overflowTopControl(overflow) {
-        this.setState({
-            overflowTop: overflow
-        })
-    }
-
-    setScrollTop(scrollTop) {
-        this.setState({
-            scrollTop: scrollTop
-        })
-    }
-
-    setBaseScrollTop(scrollTop) {
-        this.setState({
-            baseScrollTop: scrollTop
-        })
-    }
-
-    getMenuContent(content) {
-        this.menuContent = content;
-    }
-
-    scrollUp() {
-        let scrollTop = this.state.scrollTop - SCROLL_UNIT
-        if (scrollTop < 0) {
-            scrollTop = 0
-        }
-        this.setScrollTop(scrollTop)
-        this.slideScroll(scrollTop)
-    }
-
-    scrollDown() {
-        const el = ReactDOM.findDOMNode(this.menuContent)
-        let scrollTop = this.state.scrollTop + SCROLL_UNIT
-        if (scrollTop > el.scrollHeight - el.offsetHeight) {
-            scrollTop = el.scrollHeight - el.offsetHeight
-        }
-        this.setScrollTop(scrollTop)
-        this.slideScroll(scrollTop)
-    }
-
-    slideScroll(scrollTop) {
-        const el = ReactDOM.findDOMNode(this.menuContent)
-        let time = setInterval( () => {
-            if (el.scrollTop < scrollTop) {
-                el.scrollTop += 40;
-            } else if (el.scrollTop === scrollTop) {
-                clearInterval(time)
-            } else if (el.scrollTop > scrollTop) {
-                el.scrollTop -= 40;
-            }
-        }, 5)
-    }
-
-    handleCheck(command, checked) {
-        let checkItem = this.state.checkItem;
-        if (checked) {
-            checkItem.push(command)
-        } else {
-            checkItem.splice(_.indexOf(command), 1)
-        }
-        this.setState({
-            checkItem: checkItem
-        })
-
-        if (this.props.onCheck) {
-            this.props.onCheck(checkItem)
-        }
-    }
-
-    render() {
-        const {className, children, style} = this.props
-        const {overflowBottom, overflowTop, scrollTop, selected} = this.state
-
-
-        let cx = classNames.bind(menuClass);
-
-        const menuClasses = cx('u-menu', className, {
-          'pdb-change': overflowBottom,
-          'pdt-change': overflowTop
-        });
-        
-        console.log(menuClasses)
-
-
-        const childrenWithProps = React.Children.map(children, (child, index) => {
-            if (!child) {
-              return null;
-            }
-      
-            return React.cloneElement(
-              child,
-              Object.assign({}, child.props, {
-                selected: selected
-              })
-            );
-          });
-
-
-        return (
-            <div className={menuClasses} style={style}>
-                {
-                    overflowTop && (
-                        <div className={cx('handle-top')} onClick={this.scrollUp.bind(this)}>
-                            <Icon name='chevron-up'></Icon>
-                        </div>
-                    )
-                }
-                <MenuContent scrollTop={scrollTop} ref={this.getMenuContent.bind(this)}>
-                    {childrenWithProps}
-                </MenuContent>
-                {
-                    overflowBottom && (
-                        <div className={cx('handle-bottom')} onClick={this.scrollDown.bind(this)}>
-                            <Icon name='chevron-down'></Icon>
-                        </div>
-                    )
-                }
-            </div>
-        )
-    }
+    return (
+      <div className={menuClasses} style={style}>
+        {overflowTop && (
+          <div className={cx("handle-top")} onClick={this.scrollUp.bind(this)}>
+            <Icon name="chevron-up" />
+          </div>
+        )}
+        <MenuContent scrollTop={scrollTop} ref={this.getMenuContent.bind(this)}>
+          {childrenWithProps}
+        </MenuContent>
+        {overflowBottom && (
+          <div
+            className={cx("handle-bottom")}
+            onClick={this.scrollDown.bind(this)}
+          >
+            <Icon name="chevron-down" />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 Menu.propTypes = {
-    className: PropTypes.string,
-    selected: PropTypes.string,
-    withCheck: PropTypes.bool
-}
+  className: PropTypes.string,
+  selected: PropTypes.string,
+  withCheck: PropTypes.bool
+};
 
 Menu.childContextTypes = {
-    component: PropTypes.any
-}
+  component: PropTypes.any
+};
--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -2,92 +2,99 @@
  * @Author: jessica(hzgujing@corp.netease.com)
  * @Date: 2018-01-05 16:11:56
  * @Last Modified by: jessica(hzgujing@corp.netease.com)
- * @Last Modified time: 2018-05-30 19:47:11
+ * @Last Modified time: 2018-05-30 20:00:02
  */
-import React from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames/bind'
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
 
-import menuClass from './menu.scss'
+import menuClass from "./menu.scss";
 
-import MixinComponent from './MixinComponent'
-import Icon from '../icon'
+import MixinComponent from "./MixinComponent";
+import Icon from "../icon";
 
 export default class MenuItem extends MixinComponent {
-    // constructor(props) {
-    //     super (props)
-    //     // this.handleCheck = this.handleCheck.bind(this)
-    // }
+  // constructor(props) {
+  //     super (props)
+  //     // this.handleCheck = this.handleCheck.bind(this)
+  // }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.parent().setMenuItems(this)
-        }, 10)
+  componentDidMount() {
+    setTimeout(() => {
+      this.parent().setMenuItems(this);
+    }, 10);
+  }
+
+  handleItemClick() {
+    this.parent().handleMenuItemClick(this.props.command, this);
+  }
+
+  active() {
+    const parent = this.parent();
+    return parent.state.selected === this.props.command && !this.props.disabled;
+  }
+
+  render() {
+    const {
+      className,
+      children,
+      disabled,
+      divided,
+      iconName,
+      subDesc,
+      command
+    } = this.props;
+    const parent = this.parent();
+    const { tickSelect, withCheck } = parent.props;
+    const handleCheck = parent.handleCheck;
+
+    let cx = classNames.bind(menuClass);
+
+    const meneItemClass = cx("u-menu-item", className, {
+      disabled: disabled,
+      divided: divided,
+      "bg-selected": this.active() && !tickSelect && !withCheck,
+      "pdl-change": tickSelect && !withCheck
+    });
+
+    if (!withCheck) {
+      return (
+        <div
+          className={meneItemClass}
+          onClick={this.handleItemClick.bind(this)}
+        >
+          {tickSelect &&
+            this.active() && (
+              <Icon name="right-all" className={cx("tick-icon")} />
+            )}
+          {iconName && <Icon name={iconName} />}
+          {children}
+          {subDesc && <span className={cx("subtitle")}>{subDesc}</span>}
+        </div>
+      );
+    } else {
+      const childrenWithProps = React.cloneElement(children, {
+        handleCheck: handleCheck,
+        command: command
+      });
+      return (
+        <div className={meneItemClass}>
+          {childrenWithProps}
+          {subDesc && <span className={cx("subtitle")}>{subDesc}</span>}
+        </div>
+      );
     }
-
-    handleItemClick() {
-        this.parent().handleMenuItemClick(this.props.command, this)
-    }
-
-    active() {
-        const parent = this.parent()
-        return parent.state.selected === this.props.command && ! this.props.disabled
-    }
-
-    render() {
-        const {className, children, disabled, divided, iconName, subDesc, command} = this.props
-        const parent = this.parent()
-        const { tickSelect, withCheck}= parent.props
-        const handleCheck = parent.handleCheck
-
-        let cx = classNames.bind(menuClass);
-        
-        const meneItemClass = cx('u-menu-item', className, {
-            'disabled': disabled,
-            'divided': divided,
-            'bg-selected': this.active() && !tickSelect && !withCheck,
-            'pdl-change': tickSelect && !withCheck
-        })
-        
-        if (!withCheck) {
-            return (
-                <div className={meneItemClass} onClick={this.handleItemClick.bind(this)}>
-                    {
-                        tickSelect && this.active() && (
-                            <Icon name='right-all' className={cx("tick-icon")}></Icon>
-                        )
-                    }
-                    { iconName && (
-                        <Icon name={iconName}></Icon>
-                    )}
-                    {children}
-                    { subDesc && (
-                        <span className={cx("subtitle")}>{subDesc}</span>
-                    )}
-                </div>
-            )
-        } else {
-            const childrenWithProps = React.cloneElement(children, { handleCheck: handleCheck, command: command })
-            return (
-                <div className={meneItemClass}>
-                    {childrenWithProps}
-                    { subDesc && (
-                        <span className={cx("subtitle")}>{subDesc}</span>
-                    )}
-                </div>
-            )
-        }
-    }
+  }
 }
 
 MenuItem.propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.any,
-    disabled: PropTypes.bool,
-    divided: PropTypes.bool,
-    iconName: PropTypes.string,
-    subtitle: PropTypes.string,
-    tickSelect: PropTypes.string,
-    command: PropTypes.string,
-    subDesc: PropTypes.string
-}
+  className: PropTypes.string,
+  children: PropTypes.any,
+  disabled: PropTypes.bool,
+  divided: PropTypes.bool,
+  iconName: PropTypes.string,
+  subtitle: PropTypes.string,
+  tickSelect: PropTypes.string,
+  command: PropTypes.string,
+  subDesc: PropTypes.string
+};
