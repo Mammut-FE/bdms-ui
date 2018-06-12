4585434d6afa55c3c5e65e1d25dbdd3e41ed79f2
mengera88 <1158584767@qq.com>
2018-06-12 11:11:40 +0800

    tag

---
+++ src/components/tag/Tag.tsx
@@ -0,0 +1,70 @@
+import classNames from 'classnames/bind';
+import React, { Component, MouseEvent } from 'react';
+
+import Icon from '../icon/Icon';
+import styles from './tag.scss';
+
+interface ITagState {
+  visible: boolean;
+}
+
+interface ITagProps {
+  size?: string;
+  closable?: boolean;
+  iconName?: string;
+  onClose?: (e: MouseEvent<HTMLElement>) => void;
+}
+
+const cx = classNames.bind(styles);
+
+export default class Tag extends Component<ITagProps, ITagState> {
+  public static defaultProps = {
+    closable: true,
+    size: 'small'
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true
+    };
+    this.closeTag = this.closeTag.bind(this);
+  }
+
+  public closeTag(e) {
+    this.setState({ visible: false });
+    if (this.props.onClose) {
+      this.props.onClose(e);
+    }
+    e.stopPropagation();
+  }
+  public componentWillUnmount() {
+    console.log('tag component will unmount');
+  }
+  public render() {
+    const { children, size, closable, iconName } = this.props;
+    const { visible } = this.state;
+
+    const tagClass = cx('u-tag', `u-tag-${size}`);
+
+    const iconCloseStyle = {
+      marginLeft: '4px'
+    };
+
+    const iconStyle = {
+      marginRight: '4px'
+    };
+
+    if (visible) {
+      return (
+        <div className={tagClass}>
+          {iconName && <Icon name={iconName} style={iconStyle} />}
+          {children}
+          {closable && <Icon name="close" onClick={this.closeTag} style={iconCloseStyle} />}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
