804e1d292675f89c9f4a95ef5f4612edbf5a9c27
XGHeaven <xgheaven@gmail.com>
2018-11-26 17:18:12 +0800

    feat(input): add prefix/suffix of input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,16 +1,16 @@
+import cx from 'classnames';
 import * as React from 'react';
 import { Independence } from '../../lib/independence';
-import ClassNames from 'classnames/bind'
-import styles from './input.scss'
-import { Omit } from '../../lib/type'
+import { Omit } from '../../lib/type';
+import './input.scss'
 
-export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'prefix'> {
   value?: string
   onChange?: (value: string) => void
   defaultValue?: string
+  prefix?: React.ReactNode
+  suffix?: React.ReactNode
 }
-
-const cx = ClassNames.bind(styles)
 
 @Independence({
   value: {
@@ -26,7 +26,22 @@ export class Input extends React.Component<InputProps> {
   }
 
   public render() {
-    const {value, className, ...restProps} = this.props
-    return <input className={cx('u-input', className)} type="text" value={value} {...restProps} onChange={this.onChange}/>
+    const {value, className, prefix, suffix, ...restProps} = this.props
+    const needWrapper = !!(prefix || suffix)
+    const input = <input className={cx('ma-input', {
+      'ma-input_has_prefix': !!prefix,
+      'ma-input_has_suffix': !!suffix
+    }, className)} type="text" value={value} {...restProps} onChange={this.onChange}/>
+    if (needWrapper) {
+      return (
+        <span className="ma-input__wrapper">
+          {prefix ? (<span className="ma-input__prefix">{prefix}</span>) : null}
+          {input}
+          {suffix ? (<span className="ma-input__suffix">{suffix}</span>) : null}
+        </span>
+      )
+    } else {
+      return input
+    }
   }
 }
